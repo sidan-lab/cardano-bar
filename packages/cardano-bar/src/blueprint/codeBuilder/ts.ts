f8b366f8ad604c0e3d1d836af3aeeb8b11c19886
Ken Lau <kenlauhong2015@gmail.com>
2025-07-25 23:37:47 +0800

    fix: command not found

---
+++ packages/cardano-bar/src/blueprint/codeBuilder/ts.ts
@@ -0,0 +1,184 @@
+import { ICodeBuilder } from ".";
+import { capitalizedFirstLetter, snakeToCamelCase } from "../../utils";
+import { ScriptPurpose } from "../types";
+
+export class TSCodeBuilder implements ICodeBuilder {
+  typePackage = () => "@meshsdk/core";
+
+  spendJson = (
+    blueprintName: string,
+    validatorIndex: number,
+    parameters: string[],
+    datum?: string,
+    redeemer?: string
+  ) => {
+    let code = "";
+    code = code = `export class ${blueprintName} extends SpendingBlueprint {\n`;
+    code += `  compiledCode: string\n`;
+    code += `\n`;
+
+    code +=
+      parameters.length === 0
+        ? `  constructor() {\n`
+        : `  constructor(params: [${parameters.join(", ")}]) {\n`;
+    code += `    const compiledCode = blueprint.validators[${validatorIndex}]!.compiledCode;\n`;
+    code += `    super(version, networkId, stakeKeyHash, isStakeScriptCredential);\n`;
+    code += `    this.compiledCode = compiledCode\n`;
+    code +=
+      parameters.length === 0
+        ? `    this.noParamScript(compiledCode)\n`
+        : `    this.paramScript(compiledCode,  params, "JSON")\n`;
+    code += `  }\n`;
+    code += `\n`;
+    code +=
+      parameters.length > 0
+        ? this.createTypeCheckMethod("params", `[${parameters.join(", ")}]`)
+        : ``;
+    code += datum ? this.createTypeCheckMethod("datum", datum) : ``;
+    code += redeemer ? this.createTypeCheckMethod("redeemer", redeemer) : ``;
+    code += `}\n`;
+    return code;
+  };
+
+  mintJson = (
+    blueprintName: string,
+    validatorIndex: number,
+    parameters: string[]
+  ) => {
+    let code = "";
+    code = code = `export class ${blueprintName} extends MintingBlueprint {\n`;
+    code += `  compiledCode: string\n`;
+    code += `\n`;
+
+    code +=
+      parameters.length === 0
+        ? `  constructor() {\n`
+        : `  constructor(params: [${parameters.join(", ")}]) {\n`;
+    code += `    const compiledCode = blueprint.validators[${validatorIndex}]!.compiledCode;\n`;
+    code += `    super(version);\n`;
+    code += `    this.compiledCode = compiledCode\n`;
+    code +=
+      parameters.length === 0
+        ? `    this.noParamScript(compiledCode)\n`
+        : `    this.paramScript(compiledCode,  params, "JSON")\n`;
+    code += `  }\n`;
+    code += `\n`;
+    code +=
+      parameters.length > 0
+        ? this.createTypeCheckMethod("params", `[${parameters.join(", ")}]`)
+        : ``;
+    code += `}\n`;
+    return code;
+  };
+
+  withdrawJson = (
+    blueprintName: string,
+    validatorIndex: number,
+    parameters: string[]
+  ) => {
+    let code = "";
+    code =
+      code = `export class ${blueprintName} extends WithdrawalBlueprint {\n`;
+    code += `  compiledCode: string\n`;
+    code += `\n`;
+
+    code +=
+      parameters.length === 0
+        ? `  constructor() {\n`
+        : `  constructor(params: [${parameters.join(", ")}]) {\n`;
+    code += `    const compiledCode = blueprint.validators[${validatorIndex}]!.compiledCode;\n`;
+    code += `    super(version, networkId);\n`;
+    code += `    this.compiledCode = compiledCode\n`;
+    code +=
+      parameters.length === 0
+        ? `    this.noParamScript(compiledCode)\n`
+        : `    this.paramScript(compiledCode,  params, "JSON")\n`;
+    code += `  }\n`;
+    code += `\n`;
+    code +=
+      parameters.length > 0
+        ? this.createTypeCheckMethod("params", `[${parameters.join(", ")}]`)
+        : ``;
+    code += `}\n`;
+    return code;
+  };
+
+  createTypeCheckMethod = (methodName: string, data: string): string => {
+    let code = `   ${methodName} = (data: ${data}): ${data} => data\n`;
+    return code;
+  };
+
+  importJson = (importName: string, filePath: string) => {
+    let code = `import ${importName} from "${filePath}"\n`;
+    return code;
+  };
+
+  importPackage = (imports: string[], packageName: string) => {
+    let code = `import { ${imports.join(", ")} } from "${packageName}"\n`;
+    return code;
+  };
+
+  exportType = (typeName: string, typeCode: string) => {
+    return `export type ${typeName} = ${typeCode};`;
+  };
+
+  constant = (plutusVesion: string) => `const version = "${plutusVesion}"
+const networkId = 0; // 0 for testnet; 1 for mainnet`;
+
+  spendConstants = () => `
+// Every spending validator would compile into an address with an staking key hash
+// Recommend replace with your own stake key / script hash
+const stakeKeyHash = ""
+const isStakeScriptCredential = false`;
+
+  getValVariableName = (validatorTitle: string) => {
+    const valFuncName = validatorTitle.split(".");
+    return snakeToCamelCase(valFuncName[valFuncName.length - 2]);
+  };
+
+  getOrTypeCode = (types: string[]): string => types.join(" | ");
+
+  getCodeImportList = (importCode: string): string[] => importCode.split(" | ");
+
+  getBlueprintName = (blueprintName: string, purpose: ScriptPurpose): string =>
+    `${blueprintName}${capitalizedFirstLetter(purpose)}Blueprint`;
+
+  any = (): string => "any";
+
+  pairs = (key: string, value: string): string => `Pairs<${key}, ${value}>`;
+
+  list = (itemCode: string): string => `List<${itemCode}>`;
+
+  tuple = (...itemCodes: string[]): string =>
+    `Tuple<[${itemCodes.join(", ")}]>`;
+
+  option = (someCode: string): string => `Option<${someCode}>`;
+
+  constr = (
+    index: number,
+    fieldCodes: string[]
+  ): { toImport: string; constructorCode: string } => {
+    switch (index) {
+      case 0:
+        return {
+          toImport: "ConStr0",
+          constructorCode: `ConStr0<[${fieldCodes.join(", ")}]>`,
+        };
+      case 1:
+        return {
+          toImport: "ConStr1",
+          constructorCode: `ConStr1<[${fieldCodes.join(", ")}]>`,
+        };
+      case 2:
+        return {
+          toImport: "ConStr2",
+          constructorCode: `ConStr2<[${fieldCodes.join(", ")}]>`,
+        };
+      default:
+        return {
+          toImport: "ConStr",
+          constructorCode: `ConStr<${index}, [${fieldCodes.join(", ")}]>`,
+        };
+    }
+  };
+}
---
+++ packages/cardano-bar/src/blueprint/types.ts
@@ -0,0 +1,210 @@
+export type Preamble = {
+  title: string;
+  description: string;
+  version: string;
+  plutusVersion: "v1" | "v2" | "v3";
+  compiler: {
+    name: string;
+    version: string;
+  };
+  license: string;
+};
+
+export type ByteDefinition = {
+  title: string;
+  dataType: "bytes";
+};
+
+export type DataDefinition = {
+  title: "Data";
+  description: string;
+};
+
+export type BoolDefinition = {
+  title: "Bool";
+  anyOf: [
+    {
+      title: "False";
+      dataType: "constructor";
+      index: 0;
+      fields: [];
+    },
+    {
+      title: "True";
+      dataType: "constructor";
+      index: 1;
+      fields: [];
+    }
+  ];
+};
+
+export type IntDefinition = {
+  dataType: "integer";
+};
+
+export type MapDefinition = {
+  title: string;
+  dataType: "map";
+  keys: {
+    $ref: string;
+    // $ref: "#/definitions/PolicyId";
+  };
+  values: {
+    $ref: string;
+    // $ref: "#/definitions/Pairs$AssetName_Int";
+  };
+};
+
+export type ListDefinition = {
+  title: string;
+  dataType: "list";
+  items: {
+    $ref: string;
+  };
+};
+
+export type TupleDefinition = {
+  title: string;
+  dataType: "list";
+  items: {
+    $ref: string;
+  }[];
+};
+
+export type OptionDefinition = {
+  title: "Option";
+  anyOf: [
+    {
+      title: "Some";
+      description: "An optional value.";
+      dataType: "constructor";
+      index: 0;
+      fields: [
+        {
+          $ref: string;
+          // $ref: "#/definitions/StakeCredential";
+        }
+      ];
+    },
+    {
+      title: "None";
+      description: "Nothing.";
+      dataType: "constructor";
+      index: 1;
+      fields: [];
+    }
+  ];
+};
+
+export type ConstructorDefinition = {
+  title: string;
+  dataType: "constructor";
+  index: number;
+  fields: {
+    $ref: string;
+  }[];
+};
+
+export type ConstructorsDefinition = {
+  title: string;
+  description?: string;
+  anyOf: ConstructorDefinition[];
+};
+
+export type IgnoreDefinition = DataDefinition;
+
+export type CustomDefinition =
+  | MapDefinition
+  | ListDefinition
+  | TupleDefinition
+  | OptionDefinition
+  | ConstructorsDefinition;
+
+export type PrimitiveDefinition =
+  | IntDefinition
+  | ByteDefinition
+  | BoolDefinition;
+
+export type Definition = {
+  title?: string;
+  dataType?: string;
+  description?: string;
+  index?: number;
+  anyOf?:
+    | ConstructorDefinition[]
+    | [
+        {
+          title: "Some";
+          description: "An optional value.";
+          dataType: "constructor";
+          index: 0;
+          fields: [
+            {
+              $ref: string;
+              // $ref: "#/definitions/StakeCredential";
+            }
+          ];
+        },
+        {
+          title: "None";
+          description: "Nothing.";
+          dataType: "constructor";
+          index: 1;
+          fields: [];
+        }
+      ]
+    | [
+        {
+          title: "False";
+          dataType: "constructor";
+          index: 0;
+          fields: [];
+        },
+        {
+          title: "True";
+          dataType: "constructor";
+          index: 1;
+          fields: [];
+        }
+      ];
+  keys?: {
+    $ref: string;
+  };
+  values?: {
+    $ref: string;
+  };
+  items?:
+    | {
+        $ref: string;
+      }
+    | {
+        $ref: string;
+      }[];
+  fields?: {
+    $ref: string;
+  }[];
+};
+
+export type Definitions = {
+  [key: string]: Definition;
+};
+
+export type Validator = {
+  title: string;
+  redeemer: {
+    title: string;
+    schema: { $ref?: string };
+  };
+  parameters?: {
+    title: string;
+    schema: { $ref: string };
+  }[];
+  datum?: {
+    title: string;
+    schema: { $ref: string };
+  };
+  dataType: "validator";
+  code: string;
+};
+
+export type ScriptPurpose = "spend" | "mint" | "withdraw" | "publish";
